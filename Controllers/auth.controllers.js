997b82f57497181ad292848f6d75009e1b9ad144
rupatul1995 <rupalichavan578@gmail.com>
2024-07-10 15:06:27 +0530

    updata

---
+++ Controllers/auth.controllers.js
@@ -0,0 +1,17 @@
+ export const Login=(req, res)=>{
+res.sent("Login completed.")
+}
+
+export const Register=(req, res)=>{
+    res.sent("Register completed.")
+    }
+
+
+export function Register(req, res){
+        res.sent("Register completed.")
+        }
+
+
+export function Login(req, res){
+
+}
--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+import { Login, Register } from './Controllers/auth.controllers.js';
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Rupali Marathe.')
-})
+app.use(express.json());
+
+app.post('/', function (req, res) {
+  const {name, email ,password}=req.body.userData;
+  console.log(name, email ,password)
+  if(name && email && password){
+    req.send("Data recevied.");
+  }else{
+    res.send("All fields are mandatory.")
+  }
+});
+
+app.post('/register' ,Register)
+
+app.post('/login' ,Login)
+
+app.get('/hello', function(req,res){
+  res.send("Hello.");
+});
 
 app.listen(3000)
