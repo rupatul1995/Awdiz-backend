13c017dee39d7273c9381588850a39081f836598
rupatul1995 <rupalichavan578@gmail.com>
2024-07-11 15:58:49 +0530

    updata

--- Controllers/auth.controllers.js
+++ Controllers/auth.controllers.js
@@ -2,9 +2,42 @@ export const Login = (req, res) => {
     res.send("Login completed.");
   };
 
-export const Register=(req, res)=>{
-    res.send("Register completed.");
-    };
-
+  export const Register = async (req, res) => {
+    try {
+      const { name, email, password } = req.body.userData;
+      if (!name || !email || !password) {
+        return res.json({ success: false, error: "All fields are required." });
+      }
+      const isEmailExist = await User.findOne({ email: email });
+      console.log(isEmailExist,"isEmailExist");
+      if (isEmailExist) {
+        return res.json({
+          encryptedPassword,
+          success: false,
+          error: "Email is exists, please use another one.",
+        });
+      }
+      const encryptedPassword = await bcrypt.hash(password, 10);
+  
+      const newUser = new User({
+        name: name,
+        email: email,
+        password: encryptedPassword,
+      });
+  
+      const responseFromDb = await newUser.save();
+  
+      return res.json({
+        encryptedPassword,
+        isEmailExist,
+        success: true,
+        responseFromDb,
+        message: "Registeration Successfull.",
+      });
+    } catch (error) {
+      console.log(error, "error");
+      return res.json({ error: error, success: false });
+    }
+  };
 
 
--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 import express from 'express'
 import AllRoutes from "./routes/index.js";
 import dotenv from "dotenv";
+import { Mongoose } from 'mongoose';
 const app = express();
-
 dotenv.config();
-
-
-
 app.use(express.json());
 
 app.post("/", function (req, res) {
@@ -15,6 +12,9 @@ app.post("/", function (req, res) {
 
 app.use('/api/v1', AllRoutes)
 
+Mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("DB connected."));
 
 app.listen(process.env.PORT_NUMBER, () => {
     console.log(`Server is running on port ${process.env.PORT_NUMBER}.`);
