10041cef41d62b78aeace52e77dd7e52beb49528
rupatul1995 <rupalichavan578@gmail.com>
2024-08-02 20:23:10 +0530

    update

--- Controllers/user.controllers.js
+++ Controllers/user.controllers.js
@@ -61,3 +61,68 @@ export const GetAllCartProducts = async (req, res) => {
     return res.json({ success: false, error });
   }
 };
+
+
+
+
+
+export const wishlist = async (req, res) => {
+  try {
+    const { userId, productId } = req.body;
+    if (!userId || !productId) {
+      return res.json({ success: false, error: "User and product required." });
+    }
+
+    const isCartExist = await List.findOne({ user: userId });
+
+    if (!isCartExist) {
+      const newList = new List({
+        user: userId,
+        cartProducts: [productId],
+      });
+      await newList.save(); 
+      return res.json({
+        success: true,
+        message: "Product successfully added to cart.",
+      });
+    } else {
+      const isProductExist = isCartExist.listProducts.includes(productId);
+      if (isProductExist) {
+        return res.json({
+          success: true,
+          message: "Product already exist in cart.",
+        });
+      }
+      
+      const responseAfterAdd = await List.findOneAndUpdate(
+        { user: userId },
+        { $addToSet: { listProducts: productId } }
+      );
+      console.log(responseAfterAdd, "responseAfterAdd");
+      return res.json({
+        success: true,
+        message: "Product successfully added to cart.",
+      });
+    }
+  } catch (error) {
+    return res.json({ success: false, error });
+  }
+};
+
+export const GetAllWishlisProducts = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const listData = await List.findOne({ user: userId });
+    var listProducts = [];
+    console.log(listData.listProducts);
+    for (var i = 0; i <= listData?.listProducts?.length - 1; i++) {
+      const response = await Product.findById(listData?.listProducts[i]);
+      listProducts.push(response);
+    }
+
+    return res.json({ success: true, listProducts });
+  } catch (error) {
+    return res.json({ success: false, error });
+  }
+};
